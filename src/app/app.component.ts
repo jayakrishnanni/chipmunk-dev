a2bacbb88cc7368fda285f86d73833bdb8d5f2fe
JAYAKRISHNAN NI <jayakrishnanni@gmail.com>
2024-07-01 21:51:33 +0530

    commit :: filter-by

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,35 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: [''],
+      groupName: [''],
+      dateFrom: [''],
+      dateTo: [''],
+      dropdownInput1: [''],
+      dropdownInput2: [''],
+      dropdownInput3: [''],
+      dropdownInput4: ['']
+    });
+  }
+
+  applyDropdownValues() {
+    // Implement logic to apply dropdown values if needed
+    console.log('Dropdown values applied');
+  }
+
+  clearAll() {
+    this.myForm.reset();
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log('Form Submitted', this.myForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
