dce882a8330f8158282d0cbe2572a816771cae65
JAYAKRISHNAN NI <jayakrishnanni@gmail.com>
2024-07-07 17:02:47 +0530

    commit :: date pciker

--- src/app/date-pciker/date-pciker.component.ts
+++ src/app/date-pciker/date-pciker.component.ts
@@ -85,3 +85,76 @@ export class AppComponent implements OnInit {
   }
 }
 
+
+
+// date-picker.component.ts
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css'],
+  providers: [DatePipe]
+})
+export class DatePickerComponent implements OnInit {
+  @Input() initialDate: string;
+  @Output() dateChange = new EventEmitter<string>();
+
+  dateControl: FormControl;
+  formattedDate: string;
+
+  constructor(private datePipe: DatePipe) {
+    this.dateControl = new FormControl();
+  }
+
+  ngOnInit() {
+    if (this.initialDate) {
+      const date = this.parseDate(this.initialDate);
+      this.dateControl.setValue(date);
+      this.formattedDate = this.datePipe.transform(date, 'dd/MM/yyyy');
+    }
+
+    this.dateControl.valueChanges.subscribe(date => {
+      this.formattedDate = this.datePipe.transform(date, 'dd/MM/yyyy');
+      this.dateChange.emit(this.formattedDate);
+    });
+  }
+
+  parseDate(dateString: string): Date {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1; // months are zero-based
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+
+  onDateChange(event: any) {
+    const date = event.value;
+    this.formattedDate = this.datePipe.transform(date, 'dd/MM/yyyy');
+    this.dateChange.emit(this.formattedDate);
+  }
+}
+
+
+<!-- app.component.html -->
+<app-date-picker [initialDate]="initialDate" (dateChange)="onDateChange($event)"></app-date-picker>
+
+
+// app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  initialDate = '09/07/2024';
+
+  onDateChange(newDate: string) {
+    console.log('Selected Date:', newDate);
+  }
+}
+
